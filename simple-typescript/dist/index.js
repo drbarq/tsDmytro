a01a2d83372ed089da6e93e051240113dfe66fae
drbarq <J.Tustin@gmail.com>
2020-02-19 12:47:45 -0700

    Adding in rexport examples

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
-function multiply(a, b) {
-    return a * b;
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import multiply, { multiplyByTwo } from './multiply';
+var multiply_1 = __importDefault(require("./multiply"));
+// import * as multiplyModule from './multiply';
 var a = 4;
 var b = 5;
-console.log(a + " + " + b + " = " + multiply(a, b));
+console.log(a + " + " + b + " = " + multiply_1.default(a, b));
+// console.log(`${a} + ${b} = ${multiplyModule.default(a, b)}`);
+// multiplyModule.multiplyByTwo(2);
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,8 +1,10 @@
-function multiply(a: number, b: number): number {
-	return a * b;
-}
+// import multiply, { multiplyByTwo } from './multiply';
+import multiply, { multiplyByTwo as mBy2, HelloWorld } from './multiply';
+// import * as multiplyModule from './multiply';
 
 const a = 4;
 const b = 5;
 
 console.log(`${a} + ${b} = ${multiply(a, b)}`);
+// console.log(`${a} + ${b} = ${multiplyModule.default(a, b)}`);
+// multiplyModule.multiplyByTwo(2);
